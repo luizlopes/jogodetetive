d106219e83e22092fd5679e45ac0b25cc2cb0d0e
Luiz Lopes <lg.oliveira.lopes@gmail.com>
2017-06-18 19:39:34 -0300

    Rooms hide enter button when starting game

--- src/main/java/org/luizlopes/websocket/controller/RoomController.java
+++ src/main/java/org/luizlopes/websocket/controller/RoomController.java
@@ -2,6 +2,7 @@ package org.luizlopes.websocket.controller;
 
 import org.luizlopes.domain.Jogadores;
 import org.luizlopes.domain.Jogo;
+import org.luizlopes.domain.JogoStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,6 +22,7 @@ public class RoomController {
     public String home(Model model) {
         model.addAttribute("jogoStatus", jogo.getStatus());
         model.addAttribute("jogadores", jogadores.jogadores().size());
+        model.addAttribute("entrarEnabled", jogo.getStatus() == JogoStatus.PARTIDA_NAO_INICIADA);
         return "rooms";
     }
 
