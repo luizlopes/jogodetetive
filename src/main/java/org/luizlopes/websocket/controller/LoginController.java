9a5e33afc5d8b3d9d75e404678381d8f37867ad2
Luiz Lopes <lg.oliveira.lopes@gmail.com>
2017-06-20 00:09:34 -0300

    Create account

--- src/main/java/org/luizlopes/websocket/controller/LoginController.java
+++ src/main/java/org/luizlopes/websocket/controller/LoginController.java
@@ -4,6 +4,8 @@ import org.luizlopes.domain.UserRequest;
 import org.luizlopes.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,12 +17,14 @@ public class LoginController {
 	private UserService userService;
 	
     @RequestMapping(value = "/createAccount", method = RequestMethod.GET)
-    public String newAccount() {
+    public String newAccount(Model model) {
+        UserRequest newUser = new UserRequest();
+        model.addAttribute("newUser", newUser);
         return "create_account";
     }
 
     @RequestMapping(value = "/createAccount", method = RequestMethod.POST)
-    public String saveAccount(@RequestBody UserRequest newUser) {
+    public String saveAccount(@ModelAttribute(value = "newUser") UserRequest newUser) {
     	userService.create(newUser);
         return "login";
     }
